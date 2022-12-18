61bc12cdbc4ea1864fb27f27668b432a714d06a7
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-12-18 14:03:51 -0500

    add tests for user interactions

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,11 +1,43 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("Greeting component", () => {
-  test("renders Hello World as a text", () => {
+  test("renders 'Hello World' as a text", () => {
     render(<Greeting />);
     const helloWorldElement = screen.getByText("Hello World!");
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders 'good to see you; if the button was not clicked", () => {
+    render(<Greeting />);
+    const outputElement = screen.getByText("good to see you!", {
+      exact: false,
+    });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("renders 'Changed!' if the button is clicked", () => {
+    // arrange
+    render(<Greeting />);
+    // act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+    // assert
+    const outputElement = screen.getByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("does not render 'good to see you' if button is clicked", () => {
+    render(<Greeting />);
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    const previousOutput = screen.queryByText("good to see you!", {
+      exact: false,
+    });
+
+    expect(previousOutput).toBeNull();
   });
 });
 
@@ -15,3 +47,5 @@ describe("Greeting component", () => {
 // 3. assert
 
 // expect() function is globally available (as is the test() function)
+
+// note how we want to test all possible scenarios when writing tests
