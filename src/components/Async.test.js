05e564461b8b81119606494a273d5b8990167a85
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-12-18 21:17:21 -0500

    mock async test

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,9 +3,18 @@ import Async from "./Async";
 
 describe("Async component", () => {
   test("renders post if request succeeds", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => {
+        return [{ id: "p1", title: "first post" }];
+      },
+    });
     render(<Async />);
 
     const listItemElements = await screen.findAllByRole("listitem");
     expect(listItemElements).not.toHaveLength(0);
   });
 });
+
+// note how we simulate the success case (not actually sending a request to the API)
+// reduces the amount of network traffic etc.
